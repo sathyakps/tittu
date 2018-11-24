6dfabf74f738783f7b899f847cac12cd21e2769a
sathyakps <sathyakps@yahoo.com>
2018-11-25 01:12:10 +0530

    port number

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ console.log('Starting APP');
 setInterval(() => {
 	arbiration.checkCryptoDifference();
 }, 120000);
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
