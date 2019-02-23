c284914fad308e3c4e7788a33e228e6bda54877d
sathyakps <sathyakps@yahoo.com>
2019-02-23 14:09:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res)= > res.send('Hello World!' + new Date().toLocaleTimeString() + '(' + new Date().getTimezoneOffset() + ')'));
-
+app.get('/', (req, res) => {
+    res.send('Hello World!' + new Date().toLocaleTimeString() + '(' + new Date().getTimezoneOffset() + ')');
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
